a1035da6aafc030979bdc5e34be0ce9f9626889d
Reese-WP <roadiewilliam@gmail.com>
2025-07-09 14:42:55 -0400

    added sprint functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ int main()
     const int screenWidth = 1200;
     const int screenHeight = 900;
 
+    double stamina = 2000.0;
+
     InitWindow(screenWidth, screenHeight, "☭ Our Window ☭");
 
     //texture rend
@@ -41,13 +43,29 @@ int main()
             else {DisableCursor();}
         }
 
+        if(IsKeyPressed(KEY_F4))
+        {
+            if(GetScreenHeight() == screenHeight)
+            {
+                int monitor = GetCurrentMonitor();
+                SetWindowSize(GetMonitorWidth(monitor), GetMonitorHeight(monitor));
+                ToggleFullscreen();
+            }
+            else
+            {
+                int monitor = GetCurrentMonitor();
+                SetWindowSize(screenWidth, screenHeight);
+                ToggleFullscreen();
+            }
+        }
+
         if(IsCursorHidden())
         {
             UpdateCameraPro(&camera,
                 Vector3{
-                    (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f - (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,                                  // Move forward-backward
-                    (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,                               // Move right-left
-                    (IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_RIGHT_SHIFT))*0.1f - (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_CONTROL))*0.1f    // Move up-down
+                    ((IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.05f - (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.05f) * (1 + IsKeyDown(KEY_LEFT_SHIFT)),                                  // Move forward-backward
+                    ((IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.05f - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.05f) * (1 + IsKeyDown(KEY_LEFT_SHIFT)),                               // Move right-left
+                    //(IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_RIGHT_SHIFT))*0.1f - (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_CONTROL))*0.1f    // Move up-down
                 },
                 Vector3{
                     GetMouseDelta().x * 0.05f,                            // Rotation: yaw
@@ -57,20 +75,24 @@ int main()
                 0.0f);                              // Move to target (zoom)   GetMouseWheelMove()*2.0f
         }
         
+        if(IsKeyDown(KEY_LEFT_SHIFT)) stamina -= 1;
+        else if (stamina < 2000) stamina += 0.25;
+        
         BeginDrawing();
-            ClearBackground(RAYWHITE);
+            ClearBackground(DARKGRAY);
             BeginMode3D((camera));
 
-                DrawCube(Vector3{0,0,0}, 1, 1, 1, RED);
-                
+                DrawCube(Vector3{0,2,0}, 1, 2, 1, RED);
+                DrawPlane(Vector3{0,0,0},Vector2{500,500}, LIGHTGRAY);                
             EndMode3D();
 
             DrawFPS(10, 10);
-            DrawText("Press TAB to toggle cursor visibility", 10, 30, 20, DARKGRAY);
-            DrawText("Use W, A, S, D, SPACE and SHIFT to move", 10, 50, 20, DARKGRAY);
-            DrawText("Use mouse to look around", 10, 70, 20, DARKGRAY);
-            DrawText("context grid", 10, 100, 20, DARKGRAY);
-            DrawTexture(sad_, 140, 98, DARKGRAY);
+            DrawText("Press TAB to toggle cursor visibility", 10, 30, 20, BLACK);
+            DrawText("Use W, A, S, D, SPACE and SHIFT to move", 10, 50, 20, BLACK);
+            DrawText("Use mouse to look around", 10, 70, 20, BLACK);
+            //DrawText("context grid:", 10, 100, 20, DARKGRAY);
+            //DrawTexture(sad_, 140, 98, DARKGRAY);
+            DrawText(std::to_string((int)(stamina/20)).c_str(), 10, 880, 20, BLACK);
         EndDrawing();
     }
 
